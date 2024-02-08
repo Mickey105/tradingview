46480c5f5ed73fb6beba6b09fed5e3ce317df3fd
Mickey105 <aleaf5522@gmail.com>
2024-02-08 17:34:33 +0200

    first commit

---
+++ src/utilities/consts.js
@@ -0,0 +1,27 @@
+export const TRADING_VOLUME = [
+  {
+    volume: "&le; 100 BTC",
+    maker: 1,
+    taker: 1,
+  },
+  {
+    volume: "&ge; 100 BTC",
+    maker: 0.9,
+    taker: 0.9,
+  },
+  {
+    volume: "&ge; 500 BTC",
+    maker: 0.75,
+    taker: 0.75,
+  },
+  {
+    volume: "&ge; 1 000 BTC",
+    maker: 0.6,
+    taker: 0.6,
+  },
+  {
+    volume: "&ge; 5 000 BTC",
+    maker: 0.5,
+    taker: 0.5,
+  },
+];
